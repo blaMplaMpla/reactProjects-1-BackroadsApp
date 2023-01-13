6a1494138960769b0d9d6160ee54a6a62afd0142
blaMplaMpla <aleksissocial@gmail.com>
2023-01-13 03:35:36 +0200

    final v1.02

--- src/data.js
+++ src/data.js
@@ -24,17 +24,17 @@ const socialLinks =
 
 const services =
     [
-        { id: 1, title: "saving money", icon: "fa-wallet ", text: lorem.generateParagraphs(1) },
-        { id: 1, title: "endless hiking", icon: "fa-tree", text: lorem.generateParagraphs(1) },
-        { id: 1, title: "amazing comfort", icon: "fa-socks", text: lorem.generateParagraphs(1) }
+        { id: 1, title: "saving money", icon: "fa-wallet ", text: `lorem ipsum ${lorem.generateParagraphs(1)}` },
+        { id: 1, title: "endless hiking", icon: "fa-tree", text: `lorem ipsum ${lorem.generateParagraphs(1)}` },
+        { id: 1, title: "amazing comfort", icon: "fa-socks", text: `lorem ipsum ${lorem.generateParagraphs(1)}` }
     ];
 
 const tours =
     [
-        { id: 1, img: tour1Img, date: `september 15th, ${new Date().getFullYear()}`, title: "tibet advanture", text: lorem.generateParagraphs(1), destination: "china", duration: "6 days", cost: "from 2100$" },
-        { id: 2, img: tour2Img, date: `december 5th, ${new Date().getFullYear()}`, title: "best oof java", text: lorem.generateParagraphs(1), destination: "indonesia", duration: "11 days", cost: "from 1400$" },
-        { id: 3, img: tour3Img, date: `october 1st, ${new Date().getFullYear()}`, title: "explore hong kong", text: lorem.generateParagraphs(1), destination: "hong kong", duration: "8 days", cost: "5000$" },
-        { id: 4, img: tour4Img, date: `august 26th, ${new Date().getFullYear()}`, title: "kenya highlights", text: lorem.generateParagraphs(1), destination: "kenya", duration: "20 days", cost: "3300$" }
+        { id: 1, img: tour1Img, date: `september 15th, ${new Date().getFullYear()}`, title: "tibet advanture", text: `lorem ipsum ${lorem.generateParagraphs(1)}`, destination: "china", duration: "6 days", cost: "from 2100$" },
+        { id: 2, img: tour2Img, date: `december 5th, ${new Date().getFullYear()}`, title: "best oof java", text: `lorem ipsum ${lorem.generateParagraphs(1)}`, destination: "indonesia", duration: "11 days", cost: "from 1400$" },
+        { id: 3, img: tour3Img, date: `october 1st, ${new Date().getFullYear()}`, title: "explore hong kong", text: `lorem ipsum ${lorem.generateParagraphs(1)}`, destination: "hong kong", duration: "8 days", cost: "5000$" },
+        { id: 4, img: tour4Img, date: `august 26th, ${new Date().getFullYear()}`, title: "kenya highlights", text: `lorem ipsum ${lorem.generateParagraphs(1)}`, destination: "kenya", duration: "20 days", cost: "3300$" }
     ]
 
 
